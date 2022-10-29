d9958e0fe749b8f32681cafe886b108091e20c29
imsaifun <imsaifun@gmail.com>
2022-10-30 01:21:28 +0600

    working

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
     ? "http://localhost:3000"
-    : "https://jobbox-rouge.vercel.app/";
+    : "https://mongo-query.vercel.app/";
